fbf74206076f511d8d380c52c54b20c04cf9ce66
Kameron <you@example.com>
2016-10-02 22:03:43 -0700

    updated FileReverse

--- lab2/FileReverse.java
+++ lab2/FileReverse.java
@@ -1,3 +1,16 @@
+/*
+Kameronjeet Singh Gill
+1476833
+kgill2@ucsc.edu
+Partners:None
+
+This program will take in any txt file in and output the strings backward
+
+Most of main() was gotten from FileTokens which
+Charlie provided
+*/
+
+
 import java.io.*;
 import java.util.Scanner;
 class FileReverse{
@@ -15,13 +28,18 @@ class FileReverse{
     String line = in.nextLine().trim() + " ";
     String[] token = line.split("\\s+");
 
-    for(int i =0; i<token.length; i++){
-      lineNumber = token[i].length();
-      out.println(stringReverse(token[i],lineNumber));
+
+
+// Everything above was taken directly from Charlie's provide "FileToken"
+
+    for(int i =0; i<token.length; i++){               //this will iterate i from i all the way to the
+                                                      // length of whatever string in order to flip it
+      lineNumber = token[i].length();                 //this will give the length of tokaen at the index i.
+      out.println(stringReverse(token[i],lineNumber)); //will print out the character at the index I poistion of the string
     }
     }
 
-  in.close();
+  in.close();     //copied from FileToken
   out.close();
   }
 
@@ -30,8 +48,10 @@ class FileReverse{
 
 
   public static String stringReverse(String s, int n){
-    if (n>0){
-      return s.charAt(n-1)+stringReverse(s,n-1);
+    if (n>0){                                       //n must always be greater than 0 in this program
+                                                    //because u cant be at a -1 position
+      return s.charAt(n-1)+stringReverse(s,n-1);    //recursion method. Starts at right most poistion and works it way
+                                                    // down to the left most.
     }
     return "";
   }
