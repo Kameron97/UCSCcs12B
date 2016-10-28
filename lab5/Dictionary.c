9daf0c8815f7bbb69940464bd6e80f814f473235
Kameron Gill. Asus <kgill2@Ucsc.edu>
2016-10-27 23:55:43 -0700

    dic.c progress

--- lab5/Dictionary.c
+++ lab5/Dictionary.c
@@ -29,4 +29,31 @@ Node newNode(int k, int v){
   return(N);
 }
 
-N
+void freeNode (Node* pN){
+    if(pN !=NULL && *pN != NULL){
+      free(*pN);
+      *pN = NULL;
+    }
+}
+
+typedef struct Dictionary{
+    Node top;
+    int numItems;
+}Dictionary;
+
+
+Dictionary newDictionary(void){
+    Dictionary S = malloc(sizeof(DictionaryObj));
+    assert(S != NULL);
+    S->top = NULL;
+    S->numItems = 0;
+    return S;
+}
+
+void freeDictionary(Dictionary* pD){
+    if(pD != NULL && *pD != NULL){
+      if( !isEmpty(*pD)) makeEmpty(*pD);
+        free(*pD);
+      *pD = NULL;
+    }
+}
