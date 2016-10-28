6b6a3b398248e198feba534155ae89950040f1cc
Kameron Gill <kamerongill97@gmail.com>
2016-10-27 22:30:31 -0700

    basic filles

--- lab5/Dictionary.c
+++ lab5/Dictionary.c
@@ -6,3 +6,27 @@ Dictionary.c
 */
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+#include"Dictionary.h"
+
+
+typedef struct NodeObj{
+  int key;
+  int value;
+  struct NodeObj* next;
+} NodeObj;
+
+typedef NodeObj* Node;
+
+Node newNode(int k, int v){
+  Node N = malloc(sizeof(NodeObj));
+  assert(n!=NULL);
+  N->key = k;
+  N->value = v;
+  N->next = NULL;
+  return(N);
+}
+
+N
