5063161672353174ded7c7555fcbc2e33a00ccad
Kameron Gill. Q302LA <kamerongill97@gmail.c>
2016-11-16 11:19:25 -0800

    everything included final

--- pa4/Simulation.java
+++ pa4/Simulation.java
@@ -5,57 +5,45 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class Simulation{
-  public static void main(String[] args) throws IOException{
-    Scanner scan = null;
-    PrintWriter report = null;
-    PrintWriter trace = null;
-    Queue Storage1 = new Queue();
+public class Simulation {
+public static void main(String[] args) throws IOException {
+        Scanner scan = null;
+        PrintWriter report = null;
+        PrintWriter trace = null;
 
 //checks to see if command line is write
 //#1 on list
-    if (args.length != 1){
-      System.err.println("Invalid input. Please use input");
-      System.exit(1);
+        if (args.length != 1) {
+                System.err.println("Invalid Form. Forgot to use valid file to read from. Try again.");
+                System.exit(1);
 
-    }
-    //open files for reading/writing
-    //#2 on list
-    scan = new Scanner(new File(args[0]));
-    report = new PrintWriter( new FileWriter(args[1]+".rpt");
-    trace = new PrintWriter(new FileWriter(args[2]+".trc"));
-    //#3 on list
-    //reads in "m" jobs
-    int m = Integer.parseInt(scan.nextLine());
-    while(scan.hasNext()){
-        Storage1.enqueue((Job)getJob(scan));
-    }
+        }
+        //#2 on list. creating file: report and trace.
+        scan = new Scanner(new File(args[0]));
+        report = new PrintWriter(new FileWriter(args[1] + ".rpt"));
+        trace = new PrintWriter(new FileWriter(args[2] + ".trc"));
 
+        //#3 on list.  get the input of m Jobs
+        int m = getJob(scan);
 
-    //#4 on list
-    //goes from n->m-1
-    trace.println("Trace file: "+args[0]+".trc");
-    trace.println(m+ " Jobs:");
-    trace.println(Storage);
-
-
-    for(int n = 1; n<m-1; n++){
-      //#5 on list.
-      //create new queue array
-      Queue[] array = new Queue[n];
-
-
-    }
+        for ( int n = 1; n<m-1; n++){
+          Queue processor = new Queue(n);
+        }
 
 
 
-    //gotten from example.
-    //returns a new job from a scanner file.
-    public static Job getJob(Scanner in) {
-     String[] s = in.nextLine().split(" ");
-     int a = Integer.parseInt(s[0]);
-     int d = Integer.parseInt(s[1]);
-     return new Job(a, d);
-  }
 
-  }
+
+
+
+        //gotten from example.
+        //returns a new job from a scanner file.
+        public static Job getJob(Scanner in) {
+                String[] s = in.nextLine().split(" ");
+                int a = Integer.parseInt(s[0]);
+                int d = Integer.parseInt(s[1]);
+                return new Job(a, d);
+        }
+
+}
+}
